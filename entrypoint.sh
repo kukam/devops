1a86e05584fadddae3990a574b2baeeac33a1dbb
Dušan Grmela <dusan.grmela@drmax.eu>
2023-05-02 16:46:58 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-USER_UID=${USER_UID:-$(id -u)}
-USER_GID=${USER_GID:-$(id -g)}
+DEVOPS_UID=${DEVOPS_UID:-$(id -u)}
+DEVOPS_GID=${DEVOPS_GID:-$(id -g)}
 
-if [ "${USER_GID}" -ne 0 ]; then
+if [ "${DEVOPS_GID}" -ne 0 ]; then
     sed -i '/:20:/d' /etc/group
 fi
 
-addgroup -g ${USER_GID} devops
-adduser -u ${USER_UID} -g devops -h /home/devops -S -D devops
+addgroup -g ${DEVOPS_GID} devops
+adduser -u ${DEVOPS_UID} -g devops -h /home/devops -S -D -s /bin/zsh devops
 usermod -a -G wheel,devops devops
 
 # if [ -f "/home/devops/.zsh_history" ]; then
@@ -17,4 +17,4 @@ usermod -a -G wheel,devops devops
 
 rm -f /entrypoint.sh
 
-exec sudo -E -u devops $@
+exec sudo -E -u devops "$@"
